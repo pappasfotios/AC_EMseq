d884fab12044f4d09e9cc8ad1623725eb4d2bc0f
Fotios Pappas <fotis.pappas@slu.se>
2024-08-15 16:35:16 +0200

    using function

--- comethylation_decay.R
+++ comethylation_decay.R
@@ -5,9 +5,12 @@ library(GenomicRanges)
 library(dplyr)
 library(ggplot2)
 
+source("C:\\Users\\fopa0001\\Documents\\AC_EMseq\\comethylation_decay_function.R")
+
 # Import data
 bs <- readRDS("noSNPs_BSseq.rds")
-genome <- read.table(file.choose(), header = F)
+genome <- read.table("//wsl.localhost/Ubuntu/home/fotis/analysis/Genome/Salvelinus_hybrid/ncbi_dataset/data/GCF_002910315.2/GCF_002910315.2_ASM291031v2_genomic.fna.fai",
+                     header = F)
 genome$start <- rep(1,nrow(genome))
 names(genome)[1:2] <- c("chr", "end")
 genome <- genome[grep("NC", genome$chr),]
@@ -21,68 +24,61 @@ r$seqnames <- as.character(r$seqnames)
 
 cov <- bsseq::getCoverage(bs, type = "Cov")
 
-SDmask <- matrixStats::rowSds(m) > 0.1
-COVmask <- matrixStats::rowMins(cov) >= 10
+SDmask <- matrixStats::rowSds(m) > 0.05
+COVmask <- matrixStats::rowMins(cov) >= 5
 
 r <- r[complete.cases(m) & SDmask & COVmask,]
 r <- as.data.frame(r)
 m <- m[complete.cases(m) & SDmask & COVmask,]
-  
-bin_widths <- seq(10000, 1000000, by=10000)
 
-cors <- data.frame(cor = numeric(), abs_cor = numeric(), width = integer(), n = as.integer())
-
-for (j in bin_widths) {
-  
-  bins <- regioneR::createRandomRegions(nregions = 1000,
-                                        length.mean = j,
-                                        length.sd = 0,
-                                        genome = genome)
-  bins <- as.data.frame(bins)
-  bins$seqnames <- as.character(bins$seqnames)
-  
-  for (i in seq(1, nrow(bins))) {
-    meth <- m[r$seqnames == bins$seqnames[i] & r$start >= bins$start[i] & r$end <= bins$end[i], ]
-    w <- bins$width[i]
-    
-    if (!is.null(meth) && !is.null(dim(meth)) && !is.na(w) && w > 0) {
-      if (!is.na(dim(meth)[1]) && dim(meth)[1] > 5 && dim(meth)[1] > w / 5000) {
-        cometh <- cor(t(meth))
-        c <- mean(cometh[lower.tri(cometh, diag = FALSE)])
-        abs_c <- mean(abs(cometh[lower.tri(cometh, diag = FALSE)]))
-        n <- ncol(cometh)
-      } else {
-        c <- NA
-        abs_c <- NA
-        n <- NA
-      }
-    } else {
-      c <- NA
-      abs_c <- NA
-      n <- NA
-    }
-    
-    cors <- rbind(cors, data.frame(cor = c, abs_cor = abs_c, width = w, n = n))
-  }
-}
+short_range <- comethylation_decay(bin_widths = seq(100, 1000, by=100), nregions = 2000, length_sd = 0, genome = genome,
+                                 m = m, r = r, seed = 2118)
 
 
-cors <- cors[complete.cases(cors),]
-cors$width <- cors$width / 1000
+long_range <- comethylation_decay(bin_widths = seq(10000, 100000, by=10000), nregions = 1000, length_sd = 0, genome = genome,
+                                   m = m, r = r, seed = 1837)
 
-reduced <- setDT(cors)[, .(value= mean(abs_cor)),by=width]
 
-png(filename = "AbsCometh.png", width = 1200, height = 1000, res=300)
-ggplot(data=reduced, aes(x=width, y=value)) + theme_light() +
-  geom_smooth(col="darkred", lwd=1) + 
+reduced_short <- setDT(short_range)[, .(value= mean(abs_cor)),by=width]
+
+png(filename = "AbsCometh_short.png", width = 1800, height = 1800, res=300)
+ggplot(data=reduced_short, aes(x=width, y=value)) + theme_light() + 
+  geom_line(col="darkred", lwd=1) +
+  geom_smooth(col="grey20", lwd=0.7) + 
   ylab("Absolute correlation") + 
-  xlab("Bin width")
+  xlab("Bin width (Kb)")
 dev.off()
 
-png(filename = "AbsCometh.png", width = 1600, height = 1000, res=300)
-ggplot(data=cors, aes(x=width, y=cor, group=width)) + 
-  geom_boxplot(fill="#E30B5D") + xlab("Bin width (kb)") + ylab("Correlation") +
+png(filename = "ComethBars_short.png", width = 1800, height = 1800, res=300)
+ggplot(data=short_range, aes(x=width, y=cor, group=width)) + #ylim(c(-0.005,0.1)) +
+  geom_boxplot(fill="#E30B5D", outlier.shape = NA) + xlab("Bin width (Kb)") + ylab("Correlation") +
   theme_minimal()
 dev.off()
 
+png(filename = "Abs_ComethBars_short.png", width = 1800, height = 1800, res=300)
+ggplot(data=short_range, aes(x=width, y=abs_cor, group=width)) + #ylim(c(0.11,0.17)) +
+  geom_boxplot(fill="#E30B5D", outlier.shape = NA) + xlab("Bin width (Kb)") + ylab("Absolute correlation") +
+  theme_bw()
+dev.off()
 
+reduced_long <- setDT(long_range)[, .(value= mean(abs_cor)),by=width]
+
+png(filename = "AbsCometh_long.png", width = 1800, height = 1800, res=300)
+ggplot(data=reduced_long, aes(x=width, y=value)) + theme_light() + 
+  geom_line(col="darkred", lwd=1) +
+  geom_smooth(col="grey20", lwd=0.7) + 
+  ylab("Absolute correlation") + 
+  xlab("Bin width (Kb)")
+dev.off()
+
+png(filename = "ComethBars_long.png", width = 1800, height = 1800, res=300)
+ggplot(data=long_range, aes(x=width, y=cor, group=width)) + ylim(c(-0.01,0.1)) +
+  geom_boxplot(fill="#E30B5D", outlier.shape = NA) + xlab("Bin width (Kb)") + ylab("Correlation") +
+  theme_minimal()
+dev.off()
+
+png(filename = "Abs_ComethBars_long.png", width = 1800, height = 1800, res=300)
+ggplot(data=long_range, aes(x=width, y=abs_cor, group=width)) + ylim(c(0.11,0.17)) +
+  geom_boxplot(fill="#E30B5D", outlier.shape = NA) + xlab("Bin width (Kb)") + ylab("Absolute correlation") +
+  theme_bw()
+dev.off()
